fef2dc3d0bd33e0db5cf80f848274e720913696d
Christian Onyisi <krysnkem@gmail.com>
2025-08-27 16:08:14 +0100

    update: HomeViewModel StateNotifier -> Notifier

--- lib/ui/views/home/home_view_model.dart
+++ lib/ui/views/home/home_view_model.dart
@@ -3,26 +3,33 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mvvm_riverpod/services/animal_service.dart';
 import 'package:mvvm_riverpod/ui/views/home/home_state.dart';
 
-final homeViewModelProvider = StateNotifierProvider.autoDispose<HomeViewModel, HomeState>((ref) => HomeViewModel(ref));
+final homeViewModelProvider =
+    NotifierProvider.autoDispose<HomeViewModel, HomeState>(
+      () => HomeViewModel(),
+    );
 
-class HomeViewModel extends StateNotifier<HomeState> {
-  final Ref _ref;
+class HomeViewModel extends AutoDisposeNotifier<HomeState> {
+  HomeViewModel() : super();
 
-  HomeViewModel(this._ref) : super(HomeState.initial());
-  
+  @override
+  HomeState build() => HomeState.initial();
+
   Future<void> fetchHoundDogs() async {
     state = state.copyWith(uiState: UiState.loading);
 
-    final response = await _ref.read(animalServiceProvider).fetchHoundsList();
+    final response = await ref.read(animalServiceProvider).fetchHoundsList();
     response.fold(
-            (error){
-              state = state.copyWith(uiState: UiState.error, errorMessage: error.toString());
-            },
-            (data){
-              state = state.copyWith(uiState: UiState.success, houndList: data);
-            },
+      (error) {
+        state = state.copyWith(
+          uiState: UiState.error,
+          errorMessage: error.toString(),
+        );
+      },
+      (data) {
+        state = state.copyWith(uiState: UiState.success, houndList: data);
+      },
     );
   }
 }
 
-enum UiState{idle, loading, success, error}
+enum UiState { idle, loading, success, error }
